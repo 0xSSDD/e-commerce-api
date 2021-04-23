628e85d90e3d531698504e053fb9fd67a084101e
Arpan Dutta <smalldutta@gmail.com>
2021-04-23 06:50:37 +0300

    adds auth and admin middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,25 @@ const User = require('../models/user');
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 
+exports.isAuth  = (req, res, next) => {
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  if(!user) {
+    return res.status(403).json({
+      error: "Access denied."
+    });
+  }
+  next();
+};
+
+exports.isAdmin  = (req, res, next) => {
+  if(req.profile.role === 0) {
+    return res.status(403).json({
+      error: "Admin resource. Access denied."
+    });
+  }
+  next();
+};
+
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"], 
