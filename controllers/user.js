9bfc04acc2e4087aedbdf394dff79cab8cb2dba8
Arpan Dutta <smalldutta@gmail.com>
2021-04-23 05:26:38 +0300

    adds user signin using jwt

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const expressJwt = require('express-jwt'); // for authorization check
+const jwt = require('jsonwebtoken'); // to generate signed token
 const User = require('../models/user');
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
@@ -21,5 +23,38 @@ exports.signup = ( (req, res) => {
     });
 
   });
+
+});
+
+
+exports.signin = ( (req, res) => {
+  // find the user based on email
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => { 
+    if(err || !user){
+      return res.status(400).json({
+        error: "User with that Email does not exist, please signup  q" 
+      });
+    }
+
+    // if user is found make sure the email and password match
+    // create a authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+          error: "Email and password dont match"
+      });
+    }
+
+    // generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id}, process.env.JWT_SECRET);
+
+    // persist the token as t in cookie with expiry date
+    res.cookie('t', token, { expire: new Date() + 9999 });
+
+    //return response to user and token to frontend client
+    const { _id, name, email, role } = user;
+    return res.json({token, user: { _id, email, name, role } });
+
+  }); 
 
 });
