c55662acf855d7ca2184f65582ee550a13693d59
Arpan Dutta <smalldutta@gmail.com>
2021-04-23 14:39:34 +0300

    adds product validation

--- controllers/product.js
+++ controllers/product.js
@@ -14,13 +14,41 @@ exports.create = (req, res) => {
            error: "Image could not be uploaded"
          });    
     }
+
+    // check for all fields
+    const {
+      name,
+      description,
+      price,
+      category,
+      quantity,
+      shipping
+  } = fields;
+
+  if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+  ) {
+      return res.status(400).json({
+          error: "All fields are required"
+      });
+  }
+
     let product = new Product(fields);
 
     if (files.photo) {
+      if(files.photo.size > 1000000 ) {
+        return res.json({
+          error: "Image should be less than 1mb"
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
-
     product.save( (err, result) => {
       if (err) {
         return res.staus(400).json({
