3bd55cddce54e06dd48ea31a23a349e8b89f66ed
Arpan Dutta <smalldutta@gmail.com>
2021-05-04 16:43:19 +0300

    improves payment route

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -8,7 +8,7 @@ const gateway = new braintree.BraintreeGateway({
     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
     privateKey: process.env.BRAINTREE_PRIVATE_KEY
 });
-    
+
 exports.generateToken = (req, res) => {
     gateway.clientToken.generate({}, function(err, response) {
         if (err) {
@@ -17,4 +17,26 @@ exports.generateToken = (req, res) => {
             res.send(response);
         }
     });
+};
+
+exports.processPayment = (req, res) => {
+    let nonceFromTheClient = req.body.paymentMethodNonce;
+    let amountFromTheClient = req.body.amount;
+    // charge
+    let newTransaction = gateway.transaction.sale(
+        {
+            amount: amountFromTheClient,
+            paymentMethodNonce: nonceFromTheClient,
+            options: {
+                submitForSettlement: true
+            }
+        },
+        (error, result) => {
+            if (error) {
+                res.status(500).json(error);
+            } else {
+                res.json(result);
+            }
+        }
+    );
 };
